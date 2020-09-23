d4a8fdfedf13d37695b3c053c1dbe423cf46e4a1
AtilaRajkov <atilar777@gmial.com>
2020-09-23 21:18:40 +0200

    Finished setting up the frontend layout for all posts and for a single post.

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('blog.index');
 });
+
+Route::get('/blog/show', function () {
+  return view('blog.show');
+});
+
